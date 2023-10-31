e315e909f40567c8b8dbb499b5613f15949bf1aa
Marc Snuffy <149218762+snuffyxd@users.noreply.github.com>
2023-10-31 00:37:23 -0300

    feature: Setup serverless (#14)
    * chore: Subiendo codebase y setup the serverless
    
    * chore: Cambios menores
    
    * chore: Dejar de usar stage por el momento

---
+++ src/helpers/response.ts
@@ -0,0 +1,32 @@
+const response = ({
+  code = 200,
+  data = null,
+  err = null,
+  headers = {},
+}: {
+  code?: number;
+  data?: any;
+  err?: Error | [] | null | unknown;
+  headers?: any;
+}) => {
+  headers["Content-Type"] = "application/json";
+  if (!headers["Cache-Control"] && code != 404) {
+    headers["Cache-Control"] = process.env.CACHE_TTL;
+  }
+  // determinate the type of the args
+  let body: { data?: null | [] | {}; errors?: any[] | any } = { data };
+  if (err) {
+    delete body.data;
+    code = code && code != 200 ? code : 400;
+    body.errors = err instanceof Error ? { message: err.message } : err;
+  }
+
+  // @ts-ignore global cb
+  return cb(null, {
+    statusCode: code,
+    body: JSON.stringify(body),
+    headers,
+  });
+};
+
+export default response;
