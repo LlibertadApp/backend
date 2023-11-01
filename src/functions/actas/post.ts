77657b1ef633d0e5c18aeee613481e24f8454d52
Marc Snuffy <149218762+snuffyxd@users.noreply.github.com>
2023-11-01 13:18:34 -0300

    [feature]: Añadiendo nuevo endpoint para subir foto de actas (#19)
    * chore: Añadiendo nuevo endpoint para subir foto de actas
    
    * review: Cambios requeridos en review
    
    * fix: Arreglado issue con los parametros donde viene el payload
    
    * fix: Retornar un unico error en ves de un array
    
    * fix: Updated 'package.json' added 'pg' dependency
    
    ---------
    
    Co-authored-by: Juan Debandi <juudinidev@gmail.com>

---
+++ src/functions/actas/post.ts
@@ -0,0 +1,74 @@
+/// <reference path="../../symbols.d.ts" />
+import { APIGatewayEvent, Callback, Context } from "aws-lambda";
+import * as AWS from "aws-sdk";
+import response from "@/helpers/response";
+import logger from "@/helpers/logger";
+import { object, string } from "yup";
+
+type ActasInput = {
+  mesaId: string;
+  imagenActa: string;
+};
+
+type ActasResponse = {
+  mesaId: string;
+  url: string;
+};
+
+/**
+ * Usage
+ * POST /actas
+ * @param mesaId ID de la mesa auditada a la que pertenece este telegrama
+ * @param imagenActa Base64 que representa la imagen tomada por el usuario
+ */
+const log = logger("POST_ACTAS");
+export const handler = async (
+  event: APIGatewayEvent,
+  context: Context,
+  callback: Callback
+) => {
+  global.cb = callback;
+  const { BUCKET_NAME } = process.env;
+  const payload = JSON.parse(event.body!) as ActasInput;
+  try {
+    const schema = object({
+      mesaId: string().required(),
+      imagenActa: string().required(),
+    });
+
+    // valida
+    const { imagenActa, mesaId } = payload;
+    await schema.validate(payload);
+
+    // manda acta a S3
+    const base64 = Buffer.from(
+      imagenActa.replace(/^data:image\/\w+;base64,/, ""),
+      "base64"
+    );
+    const asset = `actas/${mesaId}.jpeg`;
+    const opts: AWS.S3.PutObjectRequest = {
+      Bucket: BUCKET_NAME!,
+      Key: asset,
+      ContentEncoding: "base64",
+      Body: base64,
+      ACL: "public-read",
+    };
+    const bucket = new AWS.S3();
+    await bucket.putObject(opts).promise();
+    log.info("telegrama subido a s3 correctamente");
+
+    const data: ActasResponse = {
+      mesaId,
+      url: `https://${BUCKET_NAME}/${asset}`,
+    };
+
+    // @TODO: guardar en db, encolar evento para OCRs
+    return response({
+      data,
+    });
+  } catch (err: any) {
+    return response({ err });
+  }
+};
+
+export default handler;
--- src/helpers/response.ts
+++ src/helpers/response.ts
@@ -1,4 +1,10 @@
-import HttpStatus from "./enum/http";
+import logger from "@/helpers/logger";
+
+type HttpResponse = {
+  statusCode: number;
+  body: any;
+  headers: [key: string];
+};
 
 const response = ({
   code = HttpStatus.OK,
@@ -8,19 +14,21 @@ const response = ({
 }: {
   code?: number;
   data?: any;
-  err?: Error | [] | null | unknown;
+  err?: Error | null | unknown;
   headers?: any;
-}) => {
+}): HttpResponse => {
+  const log = logger("RESPONSE");
   headers["Content-Type"] = "application/json";
   if (!headers["Cache-Control"] && code != 404) {
     headers["Cache-Control"] = process.env.CACHE_TTL;
   }
   // determinate the type of the args
-  let body: { data?: null | [] | {}; errors?: any[] | any } = { data };
+  let body: { data?: null | [] | {}; error?: any | any } = { data };
   if (err) {
+    log.error(err);
     delete body.data;
-    code = code && code != HttpStatus.OK ? code : HttpStatus.BAD_REQUEST;
-    body.errors = err instanceof Error ? { message: err.message } : err;
+    code = code && code != 200 ? code : 400;
+    body.error = err instanceof Error ? { message: err.message } : err;
   }
 
   // @ts-ignore global cb
