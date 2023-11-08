8750fb6d5898fda3d6bc2f8755360669f43b0a05
matiasfnunezdev <mattarg2@gmail.com>
2023-11-08 12:31:14 -0300

    migrar a aws sdk v3 los archivos que aun usaban v2

--- src/_core/firebase/firebase-admin-prod.ts
+++ src/_core/firebase/firebase-admin-prod.ts
@@ -1,12 +1,12 @@
-import * as AWS from 'aws-sdk';
+import * as AWS from "@aws-sdk/client-secrets-manager";
 import * as admin from 'firebase-admin';
 
-const secretsManager = new AWS.SecretsManager();
+const secretsManager = new AWS.SecretsManager({ region: process.env.AWS_REGION });
 
 async function getFirebaseServiceAccount() {
   const secretName = process.env.FIREBASE_SERVICE_ACCOUNT_SECRET_NAME;
   if (secretName) {
-    const data = await secretsManager.getSecretValue({ SecretId: secretName }).promise();
+    const data = await secretsManager.getSecretValue({ SecretId: secretName});
 
     if ('SecretString' in data && typeof data.SecretString === 'string') {
       return JSON.parse(data.SecretString);
