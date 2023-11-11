a7f4547452da5c0ee751871f5d17498e39898c81
matiasfnunezdev <mattarg2@gmail.com>
2023-11-11 15:57:22 -0300

    update to aws sdk v3

--- src/functions/sqs-ocrs/process.ts
+++ src/functions/sqs-ocrs/process.ts
@@ -1,9 +1,10 @@
-import { infraestructureErrors } from '@/_core/configs/errorConstants';
 import { SQSEvent } from 'aws-lambda';
-import { S3, DynamoDB } from 'aws-sdk';
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { infraestructureErrors } from '@/_core/configs/errorConstants';
 
-const s3 = new S3();
-const dynamoDB = new DynamoDB.DocumentClient();
+const s3Client = new S3Client({});
+const dynamoDBClient = new DynamoDBClient({});
 
 export const handler = async (event: SQSEvent): Promise<void> => {
 	const dynamoTableName = process.env.AWS_TABLA_DYNAMO_SQS_OCRS;
@@ -22,23 +23,35 @@ export const handler = async (event: SQSEvent): Promise<void> => {
 		const jsonKey = `json/${baseKey}.json`;
 		const imageKey = `images/${baseKey}.jpg`;
 
-		const jsonS3Object = await s3
-			.getObject({ Bucket: bucket, Key: jsonKey })
-			.promise();
+		const getObjectCommand = new GetObjectCommand({
+			Bucket: bucket,
+			Key: jsonKey,
+		});
+		const jsonS3Object = await s3Client.send(getObjectCommand);
 
 		if (jsonS3Object.Body) {
-			const jsonContent = jsonS3Object.Body.toString('utf-8');
+			const streamToString = (stream: any) =>
+				new Promise<string>((resolve, reject) => {
+					const chunks: Uint8Array[] = [];
+					stream.on('data', (chunk: Uint8Array) => chunks.push(chunk));
+					stream.once('end', () =>
+						resolve(Buffer.concat(chunks).toString('utf-8'))
+					);
+					stream.once('error', reject);
+				});
+
+			const jsonContent = await streamToString(jsonS3Object.Body);
 			const payload = JSON.parse(jsonContent);
 
-			await dynamoDB
-				.put({
-					TableName: dynamoTableName,
-					Item: {
-						...payload,
-						imageS3Key: imageKey,
-					},
-				})
-				.promise();
+			const putItemCommand = new PutItemCommand({
+				TableName: dynamoTableName,
+				Item: {
+					...payload,
+					imageS3Key: { S: imageKey },
+				},
+			});
+
+			await dynamoDBClient.send(putItemCommand);
 		}
 	}
 };
