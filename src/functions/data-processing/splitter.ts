acb680118d76e3e6b20d8c06a9eb6f38b55e9903
Jorge Matricali <jorgematricali@gmail.com>
2023-11-18 20:58:08 -0300

    FIX: GetObjectCommandInput

--- src/functions/data-processing/splitter.ts
+++ src/functions/data-processing/splitter.ts
@@ -46,7 +46,10 @@ export const handler = async (event: S3Event): Promise<void> => {
 		const bucketName = record.s3.bucket.name
 		const objectKey = record.s3.object.key
 
-		const getObjectCommand = new GetObjectCommand({ bucketName, objectKey })
+		const getObjectCommand = new GetObjectCommand({ 
+			Bucket: bucketName,
+			Key: objectKey
+		})
 
 		const response = await client.send(getObjectCommand)
 
